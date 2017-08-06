617e086e6ce0df7ffcf5752bf975b381e15a032a
ars <ars@git.com>
2017-08-06 19:44:18 +0530

    Added required files

---
+++ README.md
@@ -0,0 +1,31 @@
+# ARS Move Planner App
+
+This app gives the street view image and the wiki links about the Place and the city searched by the user.
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See how to run section for notes on how to deploy the project on a live system.
+
+### Prerequisites
+
+You need to have a web browser installed on your machine to view the site.
+
+### Installing
+
+In order to install chrome you can refer the following site and follow the installation process.
+[Google Support](https://support.google.com/chrome/answer/95346?co=GENIE.Platform%3DDesktop&hl=en-GB)
+
+## How to Run
+
+In order to see the website you just need to open the `index.html` file in your browser.
+
+## Authors
+
+* **Ankush Sankhe** - *AJAX, HTML, CSS, JavaScript and BootStrap scripting* 
+
+## Acknowledgments
+
+* [Street View Image API](https://developers.google.com/maps/documentation/streetview/)
+* [favicon image](https://cdn4.iconfinder.com/data/icons/iconsimple-logotypes/512/github-512.png)
+* **Udacity** for teaching basics of `AJAX`
+* Hat tip to anyone who's code was used.
---
+++ js/script.js
@@ -0,0 +1,61 @@
+
+function loadData() {
+
+    var $body = $('body');
+    var $showcaseElem = $('#imgb')
+    var $wikiElem = $('#wikipedia-links');
+    var $nytHeaderElem = $('#nytimes-header');
+    var $nytElem = $('#nytimes-articles');
+    var $greeting = $('#greeting');
+
+    // clear out old data before new request
+    $wikiElem.text("");
+    $nytElem.text("");
+
+    // load streetview
+    var streetStr = $('#street').val();
+    var cityStr = $('#city').val();
+    var address = streetStr + ',' + cityStr;
+
+    $greeting.text('So, you want to live at ' + address + '?');
+
+    // YOUR CODE GOES HERE!
+    var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=640x400&location='+ address + '';
+    /*$showcaseElem.append('<img class="ars" src="' + streetviewUrl + '">');*/
+    $showcaseElem.html('<img class="ars" src="' + streetviewUrl + '">');
+    
+/*
+
+    var nytimesUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q='+ cityStr + '&sort=newest&api-key=d4e26662e5c04c8284ed25162b7a0227'
+    $.getJSON( nytimesUrl, function( data ) {
+
+      $nytHeaderElem.text('New York Times Articles About' + cityStr);
+      articles = data.response.docs;
+      for(var i = 0; i < articles.length; i++) {
+        var article = articles[i];
+        $nytElem.append('<li class = "article">' + '<a href = "'+article.web_url+'">'
+        + article.headline.main + '</a>' + '<p>' + article.snippet + '</p>' + '</li>');
+      };
+    }).error(function(e){
+      $nytHeaderElem.text('Page could not be loaded');
+    });
+*/
+
+    var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + cityStr + '&format=json&callback=wikiCallback';
+    $.ajax({
+      url: wikiUrl,
+      dataType: "jsonp",
+      success: function(response) {
+        var articleList = response[1];
+        for(var i = 0; i < articleList.length; i++) {
+          var articleStr = articleList[i];
+          var url = 'http://en.wikipedia.org/wiki/' + articleStr;
+          $wikiElem.append('<li><a href="'+url+'">' + articleStr + '</a></li>');
+      };
+    }
+  });
+
+    return false;
+};
+
+$('#form-container').submit(loadData);
